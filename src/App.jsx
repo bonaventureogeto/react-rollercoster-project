35fddc64c07f357dc94245417ecf6722fd396821
Bonaventure Ogeto <bonaogeto1@gmail.com>
2023-07-17 20:33:19 +0300

    using Refs and useRef Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,51 @@
-import { useState } from 'react'
-import './App.css'
-import Counter from './components/Counter'
-import { Link } from 'react-router-dom'
+import { useState, useRef } from "react";
+import "./App.css";
+import Counter from "./components/Counter";
+import { Link } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0);
+  const count = useRef(0);
+  const boxRef = useRef(null);
 
-  const handleIncrement = () => setCount(count + 1)
-  const handleDecrement = () => setCount(count - 1);
+  const handleColorChange = () => {
+    // Access the DOM node using the ref.current property and set its background color to red
+    boxRef.current.style.backgroundColor = "black";
+  };
+
+  // const handleIncrement = () => setCount(count + 1);
+  // const handleDecrement = () => setCount(count - 1);
+
+  const handleIncrement = () => {
+    count.current += 1;
+    forceUpdate(); // Trigger a re-render
+  };
+  const handleDecrement = () => {
+    count.current -= 1;
+    forceUpdate(); // Trigger a re-render
+  };
+
+  const [_, setCount] = useState(0); 
+  const forceUpdate = () => {
+    // Update the state to force a re-render
+    setCount((prev) => prev + 1);
+  }
 
   return (
-    <>
+    <div ref={boxRef}>
       <h1>Hello!</h1>
       <h1>Welcome to my Application</h1>
       <Counter
         handleIncrement={handleIncrement}
         handleDecrement={handleDecrement}
-        count={count}
+        count={count.current}
       />
+      <div>
+        {/* Button to handle color change */}
+        <button className="button" onClick={handleColorChange}>
+          Change Color
+        </button>
+      </div>
       <div>
         <Link to="/display-data">Display Data</Link>
       </div>
@@ -27,8 +55,8 @@ function App() {
       <div>
         <Link to="/users">Users</Link>
       </div>
-    </>
+    </div>
   );
 }
 
-export default App
+export default App;
