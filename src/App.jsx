a396a0f27477ed575e5265535ef4253c09f45b14
Bonaventure Ogeto <bonaogeto1@gmail.com>
2023-07-18 19:52:11 +0300

    intro to state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,59 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useReducer } from "react";
 import "./App.css";
 import Counter from "./components/Counter";
 import { Link } from "react-router-dom";
 
+
+//NOTE: State is read-only. Don’t modify any objects or arrays in state: instead, 
+// return a new object or array that includes the changes you want to make.
+const countReducer = (state, action)=>{
+  switch (action.type) {
+    case 'increment':
+      return {count: state.count + 1}
+    case 'decrement':
+      return {count: state.count - 1}
+    default:
+      break;
+  }
+}
+
 function App() {
   // const [count, setCount] = useState(0);
-  const count = useRef(0);
+  // const count = useRef(0);
   const boxRef = useRef(null);
+
+  const [state, dispatch] = useReducer(countReducer, {count: 0});
 
   const handleColorChange = () => {
     // Access the DOM node using the ref.current property and set its background color to red
     boxRef.current.style.backgroundColor = "black";
   };
 
+  const handleIncrement = () => {
+    dispatch({type: 'increment'})
+  }
+
+  const handleDecrement = () => {
+    dispatch({type: 'decrement'})
+  }
+
   // const handleIncrement = () => setCount(count + 1);
   // const handleDecrement = () => setCount(count - 1);
 
-  const handleIncrement = () => {
-    count.current += 1;
-    forceUpdate(); // Trigger a re-render
-  };
-  const handleDecrement = () => {
-    count.current -= 1;
-    forceUpdate(); // Trigger a re-render
-  };
+  // const handleIncrement = () => {
+  //   count.current += 1;
+  //   forceUpdate(); // Trigger a re-render
+  // };
+  // const handleDecrement = () => {
+  //   count.current -= 1;
+  //   forceUpdate(); // Trigger a re-render
+  // };
 
-  const [_, setCount] = useState(0); 
-  const forceUpdate = () => {
-    // Update the state to force a re-render
-    setCount((prev) => prev + 1);
-  }
+  // const [_, setCount] = useState(0); 
+  // const forceUpdate = () => {
+  //   // Update the state to force a re-render
+  //   setCount((prev) => prev + 1);
+  // }
 
   return (
     <div ref={boxRef}>
@@ -38,7 +62,7 @@ function App() {
       <Counter
         handleIncrement={handleIncrement}
         handleDecrement={handleDecrement}
-        count={count.current}
+        count={state.count}
       />
       <div>
         {/* Button to handle color change */}
